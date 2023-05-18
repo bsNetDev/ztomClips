5dbe52a9fd5a05cbc19638fa345ba0a84a295b5a
Bryan Sessions <bryan.sessions@parknationalbank.com>
2023-05-18 07:47:32 -0400

    Initial commit and push.

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ModalComponent } from './modal/modal.component';
+import { TabsContainerComponent } from './tabs-container/tabs-container.component';
+//import { ModalService } from '../services/modal.service';
+
+@NgModule({
+  declarations: [
+    ModalComponent,
+    TabsContainerComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ModalComponent
+  ],
+  // providers: [
+  //   ModalService
+  // ]
+})
+export class SharedModule { }
