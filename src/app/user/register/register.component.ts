bd727e9826ef8be1563e97bfea6e662e9ea931fc
Bryan Sessions <bryan.sessions@gmail.com>
2023-06-28 07:31:09 -0400

    Custom Validators

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
+import { RegisterValidators } from '../validators/register-validators';
+import { EmailTaken } from '../validators/email-taken';
+
 import IUser
  from 'src/app/models/user.model';
 @Component({
@@ -9,7 +12,7 @@ import IUser
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-  constructor(private auth: AuthService) {
+  constructor(private auth: AuthService, private emailTaken: EmailTaken) {
 
   }
 
@@ -22,7 +25,7 @@ export class RegisterComponent {
   email = new FormControl('', [
     Validators.required,
     Validators.email
-  ])
+  ], [this.emailTaken.validate])
   age = new FormControl<number | null>(null, [
     Validators.required,
     Validators.min(18),
@@ -52,7 +55,7 @@ export class RegisterComponent {
     password: this.password,
     confirm_password: this.confirm_password,
     phoneNumber: this.phoneNumber
-  })
+  }, [RegisterValidators.match('password', 'confirm_password')])
 
   async register() {
     console.log('in register');
